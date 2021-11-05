a7f933d02ecabe8d0968e74800d59b0d5860abba
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-11-04 21:28:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+/*2) Escribir una función que permita al usuario ingresar 
+5 nombres de alumnos y sus correspondientes notas de matemática, 
+física y biología en 4 arreglos distintos. Una vez finalizada la 
+carga el usuario debe poder pedir el nombre y las notas para una 
+posición en particular.
+
+Por ejemplo si invocamos a la función en cada carga sería algo
+ de este estilo:
+	arregloNombres=[“Juan”]
+	arregloNotasMatematica=[8]
+	arregloNotasFisica=[9]
+	arregloNotasBiologia=[7]
+	
+Luego, el usuario puede consultar por los valores en una posición,
+ por ejemplo, si pide los valores en la posición 0 se deberá mostrar 
+ un mensaje similar a este:
+	
+	“Las notas del alumno: Juan son, matemática: 8, física: 9, biología: 7”
+*/
+
+let funcion = function () {
+  let nombres;
+  let arregloNombres: string[] = new Array(elementos);
+  let arregloNotasMatematica: string[] = new Array(elementos);
+  let arregloNotasFisica: string[] = new Array(elementos);
+  let arregloNotasBiologia: string[] = new Array(elementos);
+};
+funcion();
